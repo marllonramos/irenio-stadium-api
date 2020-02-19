90e52c0cffe7abb81637ee0a42b593323c9cd5dd
marllonramos <87marllon@gmail.com>
2020-02-19 07:00:01 -0300

    Alteracao da connectionString

--- IrenioStadium.Infra/Persistence/DataContexts/IrenioStadiumDataContext.cs
+++ IrenioStadium.Infra/Persistence/DataContexts/IrenioStadiumDataContext.cs
@@ -14,10 +14,10 @@ namespace IrenioStadium.Infra.Persistence.DataContexts
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             // optionsBuilder.UseSqlServer(@"Server=DESKTOP-FSPEV7P\SQLEXPRESS;Database=dbis;User ID=sa;Password=123@mudar");
-            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=dbis;User ID=SA;Password=MNRMNR87@");
+            // optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=dbis;User ID=SA;Password=MNRMNR87@");
 
             // banco de hml
-            // optionsBuilder.UseSqlServer(@"Server=tcp:eu-az-sql-serv1.database.windows.net,1433;Initial Catalog=dbisbanco;Persist Security Info=False;User ID=ireniostadium;Password=!Q2w3e4r;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            optionsBuilder.UseSqlServer(@"Server=tcp:eu-az-sql-serv1.database.windows.net,1433;Initial Catalog=dbisbanco;Persist Security Info=False;User ID=ireniostadium;Password=!Q2w3e4r;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
             // banco de prd
             // optionsBuilder.UseSqlServer(@"Server=tcp:eu-az-sql-serv1.database.windows.net,1433;Initial Catalog=dbisbancoprd;Persist Security Info=False;User ID=ireniostadiumprd;Password=!Q2w3e4rprd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
